af9554bd140652b4c8a4bd8a10ae910b7c005ab6
Tianle Liu <tianlel@Tianles-MacBook-Pro.local>
2022-03-08 09:01:27 -0600

    Updated project descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,21 +7,18 @@ const data = [
     image: '/images/projects/qupcakesscene.jpg',
     date: '2022',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'Puzzle-based food serving game that teaches quantum operations. '
+      + 'Targeted at middle school students. ',
   },
   {
-    title: 'Bouncing Band+',
+    title: 'AR/VR: Bouncing Band+',
     subtitle: 'A kickstarter funded potato powered weather balloon.',
     link: '/personal-site/bouncing',
     image: '/images/projects/musicbox.jpg',
     date: '2021',
     desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
+      'Exploring a new way of making music by merging gravity, randomness, '
+      + 'and the physical environment.',
   },
   {
     title: 'Tic-Tac-Toe Robot',
@@ -30,9 +27,9 @@ const data = [
     image: '/images/projects/tictactoerobot.jpg',
     date: '2021',
     desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
+      'A smart robot that plays tic-tac-toe in a 3D environment with dumbbells. '
+      + 'Trained using Maxmin Q-learning. '
+      + 'I was in charge of the training part in this class group project.',
   },
   {
     title: 'Escape',
@@ -41,9 +38,13 @@ const data = [
     image: '/images/projects/escape.jpg',
     date: '2018',
     desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
+      'A 4-min short film crafted out of randomly selected Youtube clips. '
+      + 'I created everything other than the graphics '
+      + 'and I used Final Cut to put everything together.'
+      + ' I was not familiar with mics, recordings and building soundtracks,'
+      + ' so the sound quality is pretty poor.'
+      + ' I might redo the narration'
+      + ' in the future.',
   },
 ];
 
